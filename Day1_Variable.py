323a0c5776d818d2c24dfbb9c14280be5fa6d09b
Vishnu Chandran <vishnuc142000@gmail.com>
2025-08-24 14:40:17 +0530

    Update Day1_Variable.py
    Used a basic syntax to generating a brand name for users

--- Day1_Variable.py
+++ Day1_Variable.py
@@ -1,2 +1,16 @@
 # 100-Days-Of-code-Python
 Documenatation of Python Course - Learning 100 days of code
+
+##Brand Name Generator Project
+
+#Create a greeting for your program.
+print("Welcome to rhe brand name generator.\n")
+
+#Ask the user for the city that they grew up in and store it in a variable.
+City = input("What's the name of the city you grew up?\n")
+
+#Ask the user for the name of a pet and store it in a variable.
+  Pet_Name = input("What's your pet's name?\n")
+
+#Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + City + " " + Pet_Name)
