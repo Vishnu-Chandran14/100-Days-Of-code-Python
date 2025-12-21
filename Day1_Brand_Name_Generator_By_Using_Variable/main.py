f7a10c3fd2245e3ccef456bae98dfd63edc6ea19
Vishnu-Chandran14 <vishnuc142000@gmail.com>
2025-12-21 17:48:52 +0530

    Day1 file added

---
+++ Day1_Brand_Name_Generator_By_Using_Variable/main.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator.")
+
+city_name = str(input("What's the name of the city you grew up in?"))
+
+pet_name = str(input("What's your pet's name?"))
+
+brand_name = city_name + " " + pet_name
+
+print(f"Your band name could be {brand_name}.")
